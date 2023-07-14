815c0e455aac2fa37b06db88563ef8c693318d2f
Josh-Darling13 <squiddarling@gmail.com>
2023-07-14 09:50:09 -0400

    started progressboard

--- src/components/KabanBoard.jsx
+++ src/components/KabanBoard.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import { DragDropContext } from "react-beautiful-dnd"
+
+export default function KabanBoard() {
+
+
+    const [completed, setCompleted] = useState([]);
+    const [incomplete, setInomplete] = useState([]);
+
+  return (
+    <DragDropContext>
+        <h2 style={{textAlign: "center"}}>Progress Board</h2>
+    </DragDropContext>
+  )
+}
 
